b2f1e57572eece8e4620a13b551b79ed14285ece
MAllena <MAllena@rosariobus.com.ar>
2023-01-03 10:25:24 -0300

    Leer las preferencias del sistema para dark-mode

--- src/js/app.js
+++ src/js/app.js
@@ -4,12 +4,28 @@ document.addEventListener('DOMContentLoaded', function(){
 });
 
 function darkMode(){
+    // preferencias del usuario si ya tiene configurado el modo en el sistema
+    const prefiereDarkMode = window.matchMedia('(prefers-color-scheme: dark)');
+    // console.log(prefiereDarkMode);
+    if (prefiereDarkMode.matches) {
+        document.body.classList.add('dark-mode');
+    } else {
+        document.body.classList.remove('dark-mode');
+    }
+
+    prefiereDarkMode.addEventListener('change', function(params) {
+        if (prefiereDarkMode.matches) {
+            document.body.classList.add('dark-mode');
+        } else {
+            document.body.classList.remove('dark-mode');
+        }
+    })
     
     const botonDarkMode = document.querySelector('.dark-mode-boton');
 
     botonDarkMode.addEventListener('click', function(){
         document.body.classList.toggle('dark-mode');
-    })
+    });
 }
 
 function eventListener(){
